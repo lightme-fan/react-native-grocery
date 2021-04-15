7ddcae4422caff21b81f41c46aa6ed2cac2cb70a
Fanilo Andriamarohaja <fanilo.and@onja.org>
2021-04-15 09:51:27 +0300

    Added component folder which contains a file

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 
 import nachos from '../data/nachos'
+
+import ListItem, { Separator } from '../component/ListItem'
+
 export default () => {
     return (
         <SafeAreaView>
-            {nachos.map((item)=>{
-
-                return <Text>{item.name}</Text>
-
-            })}
+            <ScrollView>
+                {nachos.map((item)=> (
+                    <React.Fragment key={item.id}>
+                        <ListItem name={item.name}/>
+                        <Separator />
+                    </React.Fragment>
+                ))}
+            </ScrollView>
         </SafeAreaView>
-
     )
-
 };
