9faf8e3c73befcbaa75252ac311d91bc3ced94bc
Fanilo Andriamarohaja <fanilo.and@onja.org>
2021-04-16 10:45:06 +0300

    removed item

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -8,6 +8,15 @@ import AddItem from '../component/AddItem';
 
 export default () => {
     const [ list, setList ] = useState(nachos)
+    
+    const addItem = (text) => {
+        setList([{id: uuid(), name: text}, ...list])
+    } 
+
+    const removeItem = (id) => {
+        const newList = list.filter(item => item.id !== id)
+        setList(newList)
+    }
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <KeyboardAvoidingView style={{ flex: 1 }}>
@@ -18,17 +27,15 @@ export default () => {
                             name={item.name}
                             onFavoritePress={() => alert('todo: handle favorite')}
                             isFavorite={index < 2}
-                            onAddedSwipe={() => alert('todo: on added swipe')}
-                            onDeleteSwipe={() => alert('todo: on delete swipe')}
+                            onAddedSwipe={() => romoveItem(item.id)}
+                            onDeleteSwipe={() => romoveItem(item.id)}
                         />
                     )}
                     KeyExtractor={(item) => item.id}
                     ItemSeparatorComponent={() => <Separator />}
                     ListHeaderComponent={() => (
                         <AddItem 
-                            onSubmitEditing={({ nativeEvent: { text } }) => {
-                                setList([{id: uuid(), name: text}, ...list])
-                            }}
+                            onSubmitEditing={({ nativeEvent: { text } }) => addItem(text)}
                         />
                     )}
                 />
